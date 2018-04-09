c37e08a72452c877e2ee5386a35b3289823b93cd
Azmul Hossain <azmul@finder-lbs>
2018-04-09 19:09:53 +0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const passort = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').GoogleStrategy;
+
 const app = express();
+passort.use(new GoogleStrategy());
 
 app.get('/',(req,res)=>{
    res.send({hi:'Buddy'})
