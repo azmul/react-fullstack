7165029b81e41ad97dc534ef140df38d75623160
Azmul Hossain <azmul@finder-lbs>
2018-04-11 16:42:42 +0600

    google auth done

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
 app.get('/',(req,res)=>{
    res.send({hi:'Buddy'})
 })
 
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken)=>{
+        console.log(accessToken);
+       }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google',{
+       scope:['profile','email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, ()=>{
+    console.log('Server running on 5000 port')
+});
