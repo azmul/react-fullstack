a69edcb10dfcefb8d11dcba45566c001db65ba62
Azmul Hossain <azmul@finder-lbs>
2018-04-17 18:46:31 +0600

    passport authenticate done

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
 const keys = require('./config/keys');
+require('./models/Users');
+require('./services/passport');
 
-app.get('/',(req,res)=>{
-   res.send({hi:'Buddy'})
-})
+const app = express();
+mongoose.connect(keys.mongoURI);
 
-passport.use(new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken)=>{
-        console.log(accessToken);
-       }
-    )
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google',{
-       scope:['profile','email']
+app.use(
+    cookieSession({
+        maxAge: 2592000000,
+        keys: [keys.cookieKey]
     })
-);
+)
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
