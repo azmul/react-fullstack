b38d089194c8bc911fc5628e4209f5d50b692e88
Azmul Hossain <azmul@finder-lbs>
2018-04-25 18:13:00 +0600

    add reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,10 @@
 import React,{Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const header = () => <h2>Header</h2>
+const dasboard = () => <h2>dasboard</h2>
+const surveyLink = () => <h2>surveyLink</h2>
+const landing = () => <h2>landing</h2>
 
 class App extends Component{
   constructor(props){
@@ -7,10 +13,14 @@ class App extends Component{
   render(){
       return(
          <div>
-             Hi there
+             <BrowserRouter>
+               <div>
+                   <Route path="/" component={landing} />
+                   <Route path="/surveys" component={surveyLink} />
+               </div>
+             </BrowserRouter>
          </div>
       )   
   }
 }
-
 export default App;
